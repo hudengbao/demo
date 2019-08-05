47f2613670d1908d4067adc5197d5343b800105d
hudengbao <285003281@qq.com>
2019-08-05 18:14:01 +0800

    aa

--- src/pages/table/table.js
+++ src/pages/table/table.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Card, Table } from 'antd'
+import { Card, Table, Modal, Button } from 'antd'
 import axios from './../.././axios/index'
 
 class TablePage extends React.Component {
 
     state = {
-        dataSource:[]
+        dataSource:[],
+        selectedRowKeys:[]
     }
 
     componentDidMount(){
@@ -22,8 +23,31 @@ class TablePage extends React.Component {
         }).then(res=>{
 
             this.setState({
-                dataSource: res
+                dataSource: res,
+                selectedRowKeys: [],
+                selectedRows: []
             })
+
+
+            console.log(this.state.selectedRowKeys)
+        })
+    }
+
+    delClick=()=>{
+
+        let rows = this.state.selectedRows;
+        let ids = [];
+
+        rows.map((item)=>{
+            ids.push(item.id)
+        })
+
+        Modal.confirm({
+            title:'删除',
+            content:`确认删除${ids.join(",")}`,
+            onOk:()=>{
+                this.request()
+            }
         })
     }
 
@@ -44,7 +68,10 @@ class TablePage extends React.Component {
             {
                 title: '性别',
                 dataIndex: 'sex',
-                key: "sex"
+                key: "sex",
+                render(sex){
+                    return sex == 1 ? '男' :'女'
+                }
             },
             {
                 title: '生日',
@@ -55,15 +82,52 @@ class TablePage extends React.Component {
                 title: '住址',
                 dataIndex: 'address',
                 key: "address"
+            },
+            {
+                title: '状态',
+                dataIndex: 'status',
+                key: "status",
+                render(status){
+
+                    const config = {
+                        '1' :'开心',
+                        '2' :'难过',
+                        '3' :'大哭'
+                    }
+
+                    return config[status]
+                }
             }
         ];
-          
+
+        const { selectedRowKeys } = this.state;
+        
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: (selectedRowKeys, selectedRows) => {
+                this.setState({
+                    selectedRowKeys, 
+                    selectedRows
+                })
+            }
+        };
 
         return (
             <div>
                 <Card>
                     <Table
                         bordered 
+                        dataSource={this.state.dataSource} 
+                        columns={columns} 
+                    />
+                </Card>
+                <Card>
+                    <div style={{marginBottom:20}}>
+                        <Button type="primary" onClick={this.delClick}>删除</Button>
+                    </div>
+                    <Table
+                        bordered 
+                        rowSelection={rowSelection}
                         dataSource={this.state.dataSource} 
                         columns={columns} 
                     />
